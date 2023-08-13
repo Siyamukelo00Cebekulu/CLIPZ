ac1f538bfe6603f552492961097c5887790c9a96
AzureAD\SiyamukeloCebekulu <siyamukelo.cebekulu@enerweb.co.za>
2023-08-13 15:30:12 +0200

    toggle tabs

--- clips/src/app/shared/shared.module.ts
+++ clips/src/app/shared/shared.module.ts
@@ -1,19 +1,24 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ModalComponent } from './modal/modal.component';
-
-
+import { TabComponent } from './tab/tab.component';
+import { TabContainerComponent } from './tab-container/tab-container.component';
 
 @NgModule({
   declarations: [
-    ModalComponent
+    ModalComponent,
+    TabComponent,
+    TabContainerComponent
   ],
   imports: [
     CommonModule
   ],
   exports:
   [
-    ModalComponent
-  ]
+    ModalComponent,
+    TabContainerComponent,
+    TabComponent
+  ],
+  
 })
 export class SharedModule { }
